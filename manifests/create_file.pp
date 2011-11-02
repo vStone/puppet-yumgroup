f5b294992ffe36f0ec4c93f4a7b8f22011764beb
Jan Vansteenkiste <jan@vstone.eu>
2011-11-02 13:11:07 +0100

    Use name in stead of descr so that the Placeholder is overwritten

--- manifests/create_file.pp
+++ manifests/create_file.pp
@@ -2,7 +2,7 @@ define yumgroup::create_file ($target) {
 
   exec {"add_repo_${name}_to_${target}":
     command     =>
-      "echo -e '[${name}]\ndescr=Placeholder for ${name}' >> ${target}",
+      "echo -e '[${name}]\nname=Placeholder for ${name}' >> ${target}",
     unless      => "grep '\\[${name}\\]' ${target}",
   }
 
